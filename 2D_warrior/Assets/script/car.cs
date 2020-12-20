3748416cdfc5c768b94c212e41b2e2afb36eebd7
Lowtea212014 <75553487+Lowtea212014@users.noreply.github.com>
2020-12-20 16:55:28 +0800

    程式碼練習
    20201220

--- 2D_warrior/Assets/script/car.cs
+++ 2D_warrior/Assets/script/car.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // last update 20201220
+    // 註解用雙斜
+    // 修飾詞 private：不顯示  pubulic ：顯示
+    // 指定使用 "="
+    // 字串要加雙引號
+    [Header("car data")] //標題
+    [Tooltip("height unit is meter")] //提示
+    [Range(1,12)] //範圍
+    public int height = 12;//public 顯示 int 為表示值為整數 
+    [Tooltip("weight unit is kilogram")]
+    [Range(10f,20f)]
+    public float weight = 10.5f; // float 表示值為有小數點
+    [Tooltip("brand is name")]
+    public string brand = "dio"; // string 表示字串,可以輸入字元
+    [Tooltip("car sky windows")]
+    public bool haswindows = true; //bool 值為 有 及 無 表示為 true or false
 
-    // Update is called once per frame
-    void Update()
-    {
+    [Header("color")] //顏色
+    public Color mycolor = new Color(0.1f, 0.1f, 0, 0.9f);
+    public Color red = Color.red;
+
+    [Header("座標")]
+        public Vector2 V2one = Vector2.one;
+    public Vector3 v3zero = new Vector3 (1f, 2f, 3f);
+    [Header("圖片與音效")]
+    public Sprite picture;
+    public AudioClip sound;
+
+    [Header("摺疊屬性與元件")]
+    public Transform tras;
+    public Camera CMR ;
         
-    }
+
+
+
+    
 }
---
+++ 2D_warrior/Assets/script/playerstat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class playerstat : MonoBehaviour
+{
+    [Header("speed")]
+    [Range(0,1000)]
+    public float speed = 10.5f;
+    [Header("jumphight")]
+    [Range(0, 3000)]
+    public int jumphight = 100;
+    [Header("do it on ground")]
+    [Tooltip("是否在地板上,預設為否")]
+    public bool on_ground = false;
+    [Header("子彈屬性")]
+    [Tooltip("子彈圖")]
+     public GameObject bullet;
+    [Header("子彈生成點")]
+    [Tooltip("子彈生成的地方")]
+    public Transform bulletborn;
+    [Header("子彈速度")]
+    [Tooltip("子彈飛行速度,單位是m/s")]
+    [Range(0, 5000)]
+    public int bulletspeed = 800;
+    [Header("開槍音效")]
+    [Tooltip("開槍時的音效")]
+    public AudioClip 開槍音效 ;
+    [Header("血量")]
+    [Range(0, 200)]
+    public int 血量 = 100;
+    AudioSource 音樂來源;
+    Rigidbody2D 剛體 ;
+    Animator 動畫控制器 ;
+
+
+}
