2b685defc1291fcdf92f5ebada22f497edf7f5d2
Lowtea212014 <75553487+Lowtea212014@users.noreply.github.com>
2021-01-02 14:34:08 +0800

    20210102

--- 2D_warrior/Assets/script/car.cs
+++ 2D_warrior/Assets/script/car.cs
@@ -33,9 +33,80 @@ public class car : MonoBehaviour
     [Header("摺疊屬性與元件")]
     public Transform tras;
     public Camera CMR ;
-        
+
+    private void Test()
+    {
+        // 輸出 方法
+        print("哈囉~");
+    }
+
+    // 傳回類型 int
+    // 必須傳回 整數資料
+    private int Ten()
+    {
+        // 傳回
+        return 10;
+    }
+
+    private float OnePointFive()
+    {
+        return 1.5f;
+    }
+
+    private string Name()
+    {
+        return "KID";
+    }
+
+    // 維護擴充性
+    private void DriveForward()
+    {
+        print("往前開");
+        print("開車音效");
+    }
+    private void DriveBack()
+    {
+        print("往後開");
+        print("開車音效");
+    }
+    private void DriveRight()
+    {
+        print("往右開");
+        print("開車音效");
+    }
+    private void DriveLeft()
+    {
+        print("往左開");
+        print("開車音效");
+    }
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="direction">方向</param>
+    /// <param name="sound">音效</param>
+    /// <param name="speed">速度</param>
+    private void Drive(string direction, string sound, int speed)
+    {
+        print("開車方向：" + direction);
+        print("開車音效：" + sound);
+        print("開車速度：" + speed);
+    }
+
+    /// <summary>
+    /// 開啟雨刷
+    /// </summary>
+    /// <param name="speed">雨刷速度</param>
+    /// 參數預設值：選填式參數
+    /// 選填式參數必須寫在最右方
+    private void OpenBrush(string sound, int count = 2, int speed = 50)
+    {
+        print("開雨刷");
+        print("音效：" + sound);
+        print("雨刷的速度：" + speed);
+        print("雨刷的數量：" + count);
+    }
+#endregion
 
 
 
-    
 }
